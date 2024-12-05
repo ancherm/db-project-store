1e74f20793b739058b3b64bd704d026bc8513e7d
Ancherm <andrewcherm2014@gmail.com>
2024-12-05 20:21:39 +0400

    init project

---
+++ src/main/java/ru/chermashentsev/dbproductstore/DbProductStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.chermashentsev.dbproductstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbProductStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbProductStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/chermashentsev/dbproductstore/DbProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chermashentsev.dbproductstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProductStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
